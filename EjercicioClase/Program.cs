ac246796a2ff47eb8facaf51350da7dc71a7115e
iandresw <rbejarano@unah.hn>
2024-07-27 12:25:07 -0600

    Update Program.cs
    se instancio la clase SuperHeroe  y se asignaron valores a los atributos

--- EjercicioClase/Program.cs
+++ EjercicioClase/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EjercicioClase;
+SuperHeroe superHeroe= new SuperHeroe();
+superHeroe.nombre = "LINTERNA VERDE";
+superHeroe.idetentidadSecreta = "Harold \"Hal\" Jordan";
+superHeroe.ciudad = "sector 2814,";
+superHeroe.puedeVolar = true;
+superHeroe.SuperPoder.nombre = "Maestría en Anillo de Linterna Verde";
+superHeroe.SuperPoder.descripcion = "Boxeador especializado Piloto experto";
+superHeroe.SuperPoder.nivel = 7;
+superHeroe.Imprimir();
+
+
+superHeroe.nombre = "Dark Phoenix";
+superHeroe.idetentidadSecreta = "Jean Elaine Grey-Summers";
+superHeroe.ciudad = "New York";
+superHeroe.puedeVolar = true;
+superHeroe.SuperPoder.nombre = "Mutante";
+superHeroe.SuperPoder.descripcion = "Telepatia";
+superHeroe.SuperPoder.nivel = 10;
+superHeroe.Imprimir();
+
+superHeroe.nombre = "Rogue";
+superHeroe.idetentidadSecreta = "Anna Marie LeBeau";
+superHeroe.ciudad = "Caldecott County, Mississippi";
+superHeroe.puedeVolar = false;
+superHeroe.SuperPoder.nombre = "Mutante";
+superHeroe.SuperPoder.descripcion = "Absorbe la Energia ";
+superHeroe.SuperPoder.nivel = 10;
+superHeroe.Imprimir();
