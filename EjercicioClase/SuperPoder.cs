5167a7655f4474276118910d3290681df9ee2247
iandresw <rbejarano@unah.hn>
2024-07-27 12:23:54 -0600

    Create SuperPoder.cs
    ser creo la clase super poder  con los atributos solicitados y se instancio en la clase superHeroe

---
+++ EjercicioClase/SuperPoder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase
+{
+    internal class SuperPoder
+    {
+        public string nombre {  get; set; } 
+        public string descripcion {  get; set; }
+        public int nivel {  get; set; }
+
+   
+    }
+}
